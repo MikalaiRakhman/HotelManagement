d6a224453f87de0e4983687fed42e4e6b95cb0d5
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-11-01 21:20:03 +0100

    Create into Bookings/Queries GetAllBookings. Added to Application DependensyInjection. Create new BookingsController with GET method GetAllBookings

---
+++ HotelManagement.Web/Controllers/BookingsController.cs
@@ -0,0 +1,33 @@
+﻿using HotelManagement.Application.Bookings.Queries;
+using HotelManagement.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Web.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BookingsController : Controller
+	{
+		private readonly IMediator _mediator;
+
+		public BookingsController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<List<Booking>>> GetAllBookings()
+		{
+			var query = new GetAllBookings();
+			var bookings = await _mediator.Send(query);
+
+			if (bookings is null or [])
+			{
+				return NotFound("No bookings found!");
+			}
+
+			return Ok(bookings);
+		}
+	}
+}
--- HotelManagement.Web/Program.cs
+++ HotelManagement.Web/Program.cs
@@ -1,3 +1,4 @@
+using HotelManagement.Application;
 using HotelManagement.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
