882ceaa8cd6715c325aea5281715dc21000a1b69
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-11-24 17:33:53 +0100

    Added new functions to DeleteBooking: DeleteBookingFromUserBookingsAsync, DeleteBookingFromRoomBookingsAsync

--- HotelManagement.Application/Bookings/Commands/DeleteBooking.cs
+++ HotelManagement.Application/Bookings/Commands/DeleteBooking.cs
@@ -23,8 +23,28 @@ namespace HotelManagement.Application.Bookings.Commands
 			}
 
 			_context.Bookings.Remove(entity);
+			await DeleteBookingFromUserBookingsAsync(entity.UserId, entity.Id);
+			await DeleteBookingFromRoomBookingsAsync(entity.RoomId, entity.Id);
 
 			await _context.SaveChangesAsync(cancellationToken);
+		}
+
+		private async Task DeleteBookingFromUserBookingsAsync(Guid userId, Guid bookingId)
+		{
+			var user = await _context.Users.FindAsync(userId);
+
+			var booking = user.Bookings.FirstOrDefault(x => x.Id == bookingId);
+
+			user.Bookings.Remove(booking);
+		}
+
+		private async Task DeleteBookingFromRoomBookingsAsync(Guid roomId, Guid bookingId)
+		{
+			var room = await _context.Rooms.FindAsync(roomId);
+
+			var booking = room.Bookings.FirstOrDefault(x => x.Id == bookingId);
+
+			room.Bookings.Remove(booking);
 		}
 	}
 }
