ce65b3a187129903cecc727f723de159513a365f
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-12-11 21:36:26 +0100

    Added RefreshTokens to TokenProvider

--- HotelManagement.Infrastructure/Identity/TokenProvider.cs
+++ HotelManagement.Infrastructure/Identity/TokenProvider.cs
@@ -1,6 +1,6 @@
-﻿using HotelManagement.Application.Common;
-using HotelManagement.Domain.Entities;
-using HotelManagement.Infrastructure.Data;
+﻿using HotelManagement.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
@@ -14,11 +14,13 @@ namespace HotelManagement.Infrastructure.Identity
 	{
 		private readonly IConfiguration _configuration;
 		private readonly ApplicationDbContext _context;
+		private readonly UserManager<ApplicationUser> _userManager;
 
-		public TokenProvider(IConfiguration configuration, ApplicationDbContext context)
+		public TokenProvider(IConfiguration configuration, ApplicationDbContext context, UserManager<ApplicationUser> userManager)
 		{
 			_configuration = configuration;
 			_context = context;
+			_userManager = userManager;
 		}
 
 		public string GenerateJwtToken(ApplicationUser user, IList<string> roles)
@@ -65,6 +67,39 @@ namespace HotelManagement.Infrastructure.Identity
 			await _context.SaveChangesAsync(cancellationToken);
 
 			return refreshToken.Token;
+		}
+
+		public async Task<(string newJwtToken, string newRefreshToken)> RefreshTokens (string refreshToken, CancellationToken cancellationToken)
+		{
+			var storedToken = _context.RefreshTokens.FirstOrDefault(rt => rt.Token == refreshToken);
+
+			if (storedToken == null || storedToken.Expires < DateTime.UtcNow) 
+			{
+				throw new UnauthorizedAccessException("Invalid refresh token or an old one");
+			}
+
+			var applicationUser = await _userManager.Users.FirstOrDefaultAsync(au => au.Id == storedToken.ApplicationUserId);
+
+			Guard.AgainstNull(applicationUser, nameof(applicationUser));
+
+			var roles = await _context.UserRoles
+				.Where(ur => ur.UserId == applicationUser.Id)
+				.Join(_context.Roles,
+						ur => ur.RoleId,
+						role => role.Id,
+						(ur, role) => role.Name)
+				.ToListAsync(cancellationToken);
+
+			var newJwtToken = GenerateJwtToken(applicationUser, roles);
+			var newRefreshToken = await GenerateRefreshToken(applicationUser.Id, cancellationToken);
+
+			storedToken.Expires = DateTime.UtcNow;
+
+			_context.RefreshTokens.Update(storedToken);
+
+			await _context.SaveChangesAsync(cancellationToken);
+
+			return (newJwtToken, newRefreshToken);
 		}
 	}
 }
